1000955ecbde36390657dc7c0125e7ff810bbe6a
yasaswikodamarthy <yasaswi.kodamarthy@gmail.com>
2020-12-23 15:55:58 +0530

    Update server.js
    changed port

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
 const http = require('http')
 const app = require('./app')
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8082
 
 var server = http.createServer(app)
 
 server.listen(port)
 
-console.log("listening to port 8080!!")
+console.log("listening to port 8082!!")
